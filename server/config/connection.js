e72f5f9d6800695fa8ab4897b43fca04dc204851
dustinturp <dustinturp@outlook.com>
2022-01-15 11:18:23 -0600

    modified connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost/pamperedpantry', {
+mongoose.connect(process.env.MONGO_DB || 'mongodb://localhost:27017/pamperedpantry', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
