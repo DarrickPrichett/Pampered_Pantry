455bbeb706e61f0011dd64c36e0f93e4df4aeabc
DarrickPrichett <darrickprichett@gmail.com>
2022-01-14 20:20:51 -0600

    re-save

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -39,43 +39,44 @@ const resolvers = {
 
       throw new AuthenticationError('Not logged in');
     },
-    Mutation: {
-      addUser: async (parent, args) => {
-        const user = await User.create(args);
-        const token = signToken(user);
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
-        return { token, user };
-      },
-      updateUser: async (parent, args, context) => {
-        if (context.user) {
-          return await User.findByIdAndUpdate(context.user._id, args, { new: true });
-        }
-
-        throw new AuthenticationError('Not logged in');
-      },
-      updateRecipe: async (parent, { _id, quantity }) => {
-        const decrement = Math.abs(quantity) * -1;
-
-        return await Recipe.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
-      },
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-
-        if (!user) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-
-        if (!correctPw) {
-          throw new AuthenticationError('Incorrect credentials');
-        }
-
-        const token = signToken(user);
-
-        return { token, user };
+      return { token, user };
+    },
+    updateUser: async (parent, args, context) => {
+      if (context.user) {
+        return await User.findByIdAndUpdate(context.user._id, args, { new: true });
       }
-    }
-  };
 
-  module.exports = resolvers;
+      throw new AuthenticationError('Not logged in');
+    },
+    updateRecipe: async (parent, { _id, quantity }) => {
+      const decrement = Math.abs(quantity) * -1;
+
+      return await Recipe.findByIdAndUpdate(_id, { $inc: { quantity: decrement } }, { new: true });
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    }
+  }
+};
+
+module.exports = resolvers;
