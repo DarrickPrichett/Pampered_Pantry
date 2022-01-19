3ca88d2d8282a82426ad7596b5c9c60399e8f8a4
Douglas Peterson <douglaspeterson6@gmail.com>
2022-01-19 10:33:58 -0600

    extra quote

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 import RecipeList from "../components/RecipeList";
 import CategoryMenu from "../components/CategoryMenu";
-import Paper from "'@mui/material/Paper";
+import Paper from "@mui/material/Paper";
 const Home = () => {
   return (
     <Paper elevation={3}>
